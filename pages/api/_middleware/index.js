d931ec49b953860afcc76de740b78c965689a0dd
masachi <zhangjb42@gmail.com>
2022-08-31 16:36:53 +0800

    fix

--- pages/api/_middleware/index.js
+++ pages/api/_middleware/index.js
@@ -5,9 +5,11 @@ const telegraphBaseUrl = "https://telegra.ph";
 export async function middleware(req, ev) {
     const { pathname, search, hash } = req.nextUrl;
 
-    if (pathname.startsWith("/file")) {
-        if(/(.*\/)*.+\.(png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP|JPEG)/g.test(pathname)) {
-            return fetch(`${telegraphBaseUrl}/${pathname}`);
+    if(pathname.startsWith("/api")) {
+        if (pathname.startsWith("/api/file")) {
+            if(/(.*\/)*.+\.(png|jpg|gif|bmp|jpeg|PNG|JPG|GIF|BMP|JPEG)/g.test(pathname)) {
+                return fetch(`${telegraphBaseUrl}/${pathname.replace("/api", "")}`);
+            }
         }
     }
 
